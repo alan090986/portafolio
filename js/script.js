3b11f39a548d68b1c37ee225fae1294f32e6ab74
alan090986 <49327642+alan090986@users.noreply.github.com>
2025-06-10 19:17:06 -0600

    intall app

--- js/script.js
+++ js/script.js
@@ -17,3 +17,31 @@ if ('serviceWorker' in navigator) {
     return true;
   }
   
+
+  let deferredPrompt;
+    const installBtn = document.getElementById('installButton');
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      deferredPrompt = e;
+      installBtn.style.display = 'block';
+    });
+
+    installBtn.addEventListener('click', () => {
+      if (deferredPrompt) {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('Usuario aceptó instalar');
+          } else {
+            console.log('Usuario rechazó');
+          }
+          deferredPrompt = null;
+        });
+      }
+    });
+
+    window.addEventListener('appinstalled', () => {
+      console.log('PWA fue instalada');
+      installBtn.style.display = 'none';
+    });
